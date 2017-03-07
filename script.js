288119e25113d0233f7f58efc34887fce41ee9f6
unknown <estudiante@LAB0248.SERVERINT.LOCAL>
2017-03-07 12:16:28 -0600

    este es el examen final

---
+++ script.js
@@ -0,0 +1,9 @@
+var BacoA  = angular.module("BacoA",[]);
+BacoA.controller("mensaje",function($scope){
+	$scope.n ="este examen esta bueno"
+});
+BacoA.controller("nombre",function($scope){
+	$scope.m ="Mynor Lemus."
+});
+
+	 
